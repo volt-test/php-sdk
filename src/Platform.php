bceb0a0c740cd01a3a36b4d6cbe3797b16281fa4
elwafa <eslamaboelwafa@gmail.com>
2025-01-29 17:36:12 +0200

    update engine version

--- src/Platform.php
+++ src/Platform.php
@@ -6,7 +6,7 @@ class Platform
 {
     private const BINARY_NAME = 'volt-test';
 
-    private const ENGINE_CURRENT_VERSION = 'v0.0.1';
+    private const ENGINE_CURRENT_VERSION = 'v0.1.0';
     private const BASE_DOWNLOAD_URL = 'https://github.com/volt-test/binaries/releases/download';
     private const SUPPORTED_PLATFORMS = [
         'linux-amd64' => 'volt-test-linux-amd64',
@@ -213,5 +213,18 @@ class Platform
         }
 
         return $binaryPath;
+    }
+
+    private static function needsUpdate(): bool
+    {
+        $binDir = self::getBinaryDir();
+        $versionFile = $binDir . '/.volt-test-version';
+
+        if (!file_exists($versionFile)) {
+            return true;
+        }
+
+        $currentVersion = trim(file_get_contents($versionFile));
+        return $currentVersion !== self::ENGINE_CURRENT_VERSION;
     }
 }
